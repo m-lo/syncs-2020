acf40242a08f0b046bb848873fb8918855416724
Marcus Lo <marcus@Marcuss-MacBook-Pro.local>
2020-08-29 17:54:09 +1000

    messages websocket

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
+import io from 'socket.io-client'
 
-export default function Chat () {
-    const [messages, setMessages] = useState([])
-    const socket = 
-    return (
-        <pre>hello</pre>
-    )
+export default function Chat({ username }) {
+  const [messages, setMessages] = useState([])
+  const addMessage = (message) => {
+    setMessages([...messages, message])
+  }
+  const socket = io('http://localhost:5002', { query: `username={username}` }).connect()
+
+  socket.on('server:message', addMessage)
+  return (
+    <pre>hello</pre>
+  )
 }
