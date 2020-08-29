2f55a58cb472f8a01f938c0cf67b9acd739aa8d4
An Thy <anthytran7@gmail.com>
2020-08-29 14:05:25 +1000

    IBM APIKEY not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import ToneAnalyzer from 'ibm-watson/tone-analyzer/v3';
 
 function App() {
+  //const API_KEY = process.env.REACT_APP_IBM_API_KEY
+  const toneAnalyzer = new ToneAnalyzer({
+    version: '2017-09-21',
+    iam_apikey: 'o0l0OuMGdb1matKG5cVH1y1aeJYa2MVumkXEYoUGGShN',
+    url: 'https://api.au-syd.tone-analyzer.watson.cloud.ibm.com/instances/61f8a1f0-1263-4c20-8215-ad302d4631fc'
+  })
+  const text = "Good morning everyone. I see that our quarter four metrics are lower than expected. I know we have had a tough year and moral is down. Can we think of ways to increase our performance next year";
+  const toneParams = {
+    tone_input: { 'text': text },
+    content_type: 'application/json',
+    };
+  toneAnalyzer.tone(toneParams)
+  .then(toneAnalysis => {
+  console.log(JSON.stringify(toneAnalysis, null, 2));
+  })
+  .catch(err => {
+  console.log('error:', err);
+  });
   return (
     <div>hi</div>
   );
