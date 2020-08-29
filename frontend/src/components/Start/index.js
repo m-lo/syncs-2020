0451d7c3fbf73044390d28bae05a0d271834f996
Marcus Lo <marcus@Marcuss-MacBook-Pro.local>
2020-08-29 16:25:27 +1000

    push

--- frontend/src/components/Start/index.js
+++ frontend/src/components/Start/index.js
@@ -1,22 +1,38 @@
-import React, { useState } from 'react'
-
+import React, { Fragment, useState } from 'react'
 import Chat from '../Chat'
 import Button from '@atlaskit/button'
-import Form, { Field, FormFooter } from '@atlaskit/form'
+import Form, { Field, FormFooter, HelperMessage } from '@atlaskit/form'
 import Textfield from '@atlaskit/textfield'
 
 
 export default function Start() {
   const [user, setUser] = useState()
   return (
-    user ? <Chat user={user} /> :
-      <Form onSubmit={(formState) => console.log('form submitted', formState)}>
-        <Field name="example-text" defaultValue="a default value">
-          <Textfield />
-        </Field>
-        <FormFooter>
-          <Button type="submit" appearance="primary">Submit</Button>
-        </FormFooter>
-      </Form>
-  )
-}
+    user ? <Chat user={user} /> : <Form
+      onSubmit={(formState) =>
+        console.log('form submitted', formState)
+      }
+    >
+      {({ formProps }) => (
+        <form {...formProps}>
+          <Field name="username">
+            {({ fieldProps }) => (
+              <Fragment>
+                <Textfield {...fieldProps} />
+              </Fragment>
+            )}
+          </Field>
+          <Field name="password">
+            {({ fieldProps }) => (
+              <Fragment>
+                <Textfield {...fieldProps} />
+              </Fragment>
+            )}
+          </Field>
+          <FormFooter>
+            <Button type="submit" appearance="primary">Login</Button>
+          </FormFooter>
+        </form>
+      )}
+    </Form>
+  )}
