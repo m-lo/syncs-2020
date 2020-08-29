0817aa2061985f5350f49e36852d6c4160bdaae4
An Thy <anthytran7@gmail.com>
2020-08-29 20:18:01 +1000

    Tone page picking colours lol

--- frontend/src/components/NewWindows/TonePage.js
+++ frontend/src/components/NewWindows/TonePage.js
@@ -12,31 +12,51 @@ function TonePage({ tone_id }) {
     const name = toneInfo[tone_id].name
     const emoji = toneInfo[tone_id].emoji 
     const description = toneInfo[tone_id].description
+    const detail = toneInfo[tone_id].detail
     
     console.log(tone_id)
     return (
         <div
           style={{
-            background: '#c7efff',
-            borderRadius: '4px',
+            backgroundImage: 'linear-gradient(#f67a36,#ed008c)',
             boxShadow: '0 1px 2px rgba(0, 0, 0, 0.2)',
             boxSizing: 'content-box',
-            fontFamily: 'Helvetica',
-            margin: '5em',
+            fontFamily: 'Menlo',
+            margin: '2em 20em 2em 20em',
             height: '60vh',
             color: '#2a4b70'
           }}
           >
           <div
-            medium={2}>
+            style={{
+                backgroundImage: 'linear-gradient(#5da4dc,#377dc9,#3a0e37)',
+                borderRadius: '4px',
+                padding: '2em 5em 2em 5em',
+                height: '60vh',
+                color: '#2a4b70'
+              }}>
             <h1>
               { name + ' ' + emoji} 
             </h1>
-          </div>
-          <div
+            <div
+              style={{
+                  color: '#276cab'
+              }}
             >
-              { description }
+              <h3>
+                { description }
+              </h3>
+            </div>
+            <div
+              style={{
+                padding: '0em 0em 0em 0em',
+                color: '#5075fa'
+            }}
+            >
+              { detail.map(line => <h5> {line }</h5>)}
+            </div>
           </div>
+          
         </div>
     );
 }
