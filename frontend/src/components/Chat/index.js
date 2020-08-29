f205459af661790811f186758b11167b0853b33b
Marcus Lo <marcus@Marcuss-MacBook-Pro.local>
2020-08-29 18:07:09 +1000

    push

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import io from 'socket.io-client'
 
-export default function Chat({ username }) {
+export default function Chat({ username = 'marcus' }) {
   const [messages, setMessages] = useState([])
   const addMessage = (message) => {
     setMessages([...messages, message])
   }
-  const socket = io('http://localhost:5002', { query: `username={username}` }).connect()
+  const socket = io('http://localhost:5002', { query: 'username=' + username }).connect()
 
   socket.on('server:message', addMessage)
   return (
---
+++ frontend/src/stories/Chat.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Chat from '../components/Chat'
+
+export default {
+    title: 'Screens/Chat',
+    component: Chat,
+  }
+
+export const ChatStory = () => <Chat />
