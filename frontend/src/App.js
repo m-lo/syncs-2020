5febc0dea17ea61de00ea51cfbd81227b4fa1efe
Marcus Lo <marcus@Marcuss-MacBook-Pro.local>
2020-08-29 15:41:17 +1000

    rearrange

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,10 @@
-import React, { useState } from 'react'
-import ToneAnalyzer from 'ibm-watson/tone-analyzer/v3'
-import { IamAuthenticator } from 'ibm-watson/auth'
-
+import React from 'react'
+import Chat from './components/Chat'
 
 function App() {
-  const [tone, setTone] = useState()
-  const toneAnalyzer = new ToneAnalyzer({
-    version: '2017-09-21',
-    authenticator: new IamAuthenticator({
-      apikey: 'o0l0OuMGdb1matKG5cVH1y1aeJYa2MVumkXEYoUGGShN',
-      url: 'https://cors-anywhere.herokuapp.com/https://iam.cloud.ibm.com/identity/token'
-    }),
-    url: 'https://api.au-syd.tone-analyzer.watson.cloud.ibm.com/instances/61f8a1f0-1263-4c20-8215-ad302d4631fc'
-  })
-  const text = "Good morning everyone. I see that our quarter four metrics are lower than expected. I know we have had a tough year and moral is down. Can we think of ways to increase our performance next year";
-  const toneParams = {
-    toneInput: { 'text': text },
-    contentType: 'application/json',
-  };
   
-  toneAnalyzer.tone(toneParams)
-    .then(toneAnalysis => {
-      setTone(JSON.stringify(toneAnalysis, null, 2))
-    })
-    .catch(err => {
-      console.log('error:', err)
-    })
   return (
-    <pre>{tone || 'loading...'}</pre>
+    <Chat />
   );
 }
 
